a0ac189de9f72ae3751095227116cd814d453b37
Aki Tada <3615622+torinoko@users.noreply.github.com>
2026-03-22 11:40:54 +0900

    本番用の設定を追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,7 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
+  config.hosts = [ "reminder.kamazuni-marunomi.net" ]
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
