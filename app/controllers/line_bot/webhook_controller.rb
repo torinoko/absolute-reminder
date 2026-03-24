62fde991d2ea1e4a8debdae56da17995e1e9dd94
Aki Tada <3615622+torinoko@users.noreply.github.com>
2026-03-24 16:47:48 +0900

    リファクタリング

--- app/controllers/line_bot/webhook_controller.rb
+++ app/controllers/line_bot/webhook_controller.rb
@@ -5,6 +5,8 @@ module LineBot
   class WebhookController < ApplicationController
     protect_from_forgery except: :create
 
+    attr_reader :token
+
     def create
       body = request.body.read
       signature = request.env['HTTP_X_LINE_SIGNATURE']
@@ -20,31 +22,37 @@ module LineBot
         return head :bad_request
       end
 
-      events.each do |event|
-        case event
-        when Line::Bot::V2::Webhook::FollowEvent
-          uid = event.source.user_id
-          token = SecureRandom.urlsafe_base64
-          LineToken.create!(uid: uid, token: token, expires_at: 1.hour.since)
-          link_url = "#{root_url}line_bot/setup?token=#{token}&openExternalBrowser=1"
-
-          message = Line::Bot::V2::MessagingApi::TextMessage.new(
-            text: "#{application_name}へようこそ！🐦\n予定を通知するために、以下のリンクからGoogleカレンダーと連携してください。\n#{link_url}"
-          )
-
-          request_body = Line::Bot::V2::MessagingApi::ReplyMessageRequest.new(
-            reply_token: event.reply_token,
-            messages: [message]
-          )
-
-          client.reply_message(reply_message_request: request_body)
-        end
-      end
+      @token = SecureRandom.urlsafe_base64
+      create_user_profile(events)
 
       head :ok
     end
 
     private
+
+    def create_user_profile(events)
+      events.each do |event|
+        case event
+        when Line::Bot::V2::Webhook::FollowEvent
+          uid = event.source.user_id
+          LineToken.create!(uid: uid, token: token, expires_at: 1.hour.since)
+          send_message(event)
+        end
+      end
+    end
+
+    def send_message(event)
+      link_url = "#{root_url}line_bot/setup?token=#{token}&openExternalBrowser=1"
+
+      message = Line::Bot::V2::MessagingApi::TextMessage.new(
+        text: "#{application_name}へようこそ！🐦\n予定を通知するために、以下のリンクからGoogleカレンダーと連携してください。\n#{link_url}"
+      )
+      request_body = Line::Bot::V2::MessagingApi::ReplyMessageRequest.new(
+        reply_token: event.reply_token,
+        messages: [message]
+      )
+      client.reply_message(reply_message_request: request_body)
+    end
 
     def client
       @client ||= Line::Bot::V2::MessagingApi::ApiClient.new(
