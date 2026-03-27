5ed07e9784499dff88612c7c5be16e2fef8ad022
Aki Tada <3615622+torinoko@users.noreply.github.com>
2026-03-27 17:40:53 +0900

    ジョブ周りの処理を期待通り動作するよう修正

--- app/services/schedule_sync.rb
+++ app/services/schedule_sync.rb
@@ -8,10 +8,11 @@ class ScheduleSync
       events.each do |event|
         schedule = user.schedules.find_or_initialize_by(google_event_id: event.id)
         ActiveRecord::Base.transaction do
-          schedule.start_at = event.start.date_time.change(sec: 0, usec: 0)
-          schedule.summary = event.summary
-          schedule.schedule_reminders = initialize_schedule_reminders(event:, schedule:)
-          schedule.save!
+          schedule.update!(
+            start_at: event.start.date_time.change(sec: 0, usec: 0),
+            summary: event.summary,
+            schedule_reminders: schedule_reminders(event:, schedule:)
+          )
         end
         settings_schedule_notification(schedule:)
       end
@@ -19,9 +20,7 @@ class ScheduleSync
 
     private
 
-    def initialize_schedule_reminders(event:, schedule:)
-      schedule.schedule_reminders.destroy_all if change_start_at?(event:, schedule:)
-
+    def schedule_reminders(event:, schedule:)
       reminders = []
       if reminder_settings?(event:)
         event.reminders.overrides.each do |override|
@@ -29,13 +28,15 @@ class ScheduleSync
           minutes = override.minutes
           reminders << schedule.schedule_reminders.find_or_initialize_by(method:, minutes:)
         end
-      else
-        schedule.schedule_reminders.destroy_all
       end
       reminders
     end
 
     private
+
+    def reminder_settings?(event:)
+      event.reminders && event.reminders.overrides.present?
+    end
 
     def change_start_at?(event:, schedule:)
       event.start.date_time.change(sec: 0, usec: 0) != schedule.start_at_was
@@ -54,7 +55,7 @@ class ScheduleSync
         job = NotifySchedulesJob.set(wait_until: recent_time).perform_later(schedule_reminder_id: recent_reminder.id)
         recent_reminder.update!(job_id: job.job_id)
       end
-      if last_minute_time > Time.current && recent_reminder != last_minute_reminder
+      if last_minute_time > Time.current && recent_reminder.id != last_minute_reminder.id
         job = NotifySchedulesJob.set(wait_until: last_minute_time).perform_later(schedule_reminder_id: last_minute_reminder.id)
         last_minute_reminder.update!(job_id: job.job_id)
       end
@@ -66,26 +67,6 @@ class ScheduleSync
       if job_ids.present?
         SolidQueue::Job.where(active_job_id: job_ids).destroy_all
         schedule.schedule_reminders.update_all(job_id: nil)
-      end
-   end
-
-    def change_start_at?(event:, schedule:)
-      event.start.date_time.change(sec: 0, usec: 0) != schedule.start_at_was
-    end
-
-    def settings_schedule_notification(schedule:)
-      return if schedule.schedule_reminders&.blank?
-
-      recent_reminder = schedule.schedule_reminders.last
-      last_minute_reminder = schedule.schedule_reminders.first
-      recent_time = schedule.start_at - recent_reminder.minutes
-      last_minute_time = schedule.start_at - last_minute_reminder.minutes
-
-      if recent_time> Time.current && !recent_reminder.notified?
-        NotifySchedulesJob.set(wait_until: recent_time).perform_later(schedule_reminder_id: recent_reminder.id)
-      end
-      if last_minute_time> Time.current && !last_minute_reminder.notified?
-        NotifySchedulesJob.set(wait_until: last_minute_time).perform_later(schedule_reminder_id: last_minute_reminder.id)
       end
     end
   end
