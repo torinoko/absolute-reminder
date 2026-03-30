61136982195e295c5879f66f2a61dba68c7f3493
Aki Tada <3615622+torinoko@users.noreply.github.com>
2026-03-30 17:33:11 +0900

    Google認証のscopeを修正

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
            ENV['GOOGLE_CLIENT_ID'],
            ENV['GOOGLE_CLIENT_SECRET'],
-           scope: 'openid, profile, email, calendar.readonly',
+           scope: 'email, profile, https://www.googleapis.com/auth/calendar.events.readonly',
            skip_jwt: true,
            prompt: 'consent',
            access_type: 'offline'
