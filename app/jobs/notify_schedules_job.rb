719369781558a53a45d15ec1ed4e1eb3caf2f6e9
Aki Tada <3615622+torinoko@users.noreply.github.com>
2026-03-30 16:42:12 +0900

    ジョブの設定を追加

--- app/jobs/notify_schedules_job.rb
+++ app/jobs/notify_schedules_job.rb
@@ -4,16 +4,25 @@ class NotifySchedulesJob < ApplicationJob
   queue_as :default
 
   def perform(schedule_reminder_id:)
-    schedule = ScheduleReminder.find(schedule_reminder_id).schedule
-    process_notification(schedule)
+    schedule_reminder = ScheduleReminder.find(schedule_reminder_id)
+    schedule = schedule_reminder.schedule
+    start_at = schedule.start_at.strftime('%H:%M')
+    text = "#{start_at}\n#{schedule.summary}\nまであと#{time(schedule_reminder:)}だよ 🕊️"
+    process_notification(schedule:, text:)
   end
 
   private
 
-  def process_notification(schedule)
+  def time(schedule_reminder:)
+    hours = schedule_reminder.minutes / 60
+    minutes = schedule_reminder.minutes % 60
+    hours_text = "#{hours}時間" if hours > 0
+    minutes_text = "#{minutes}分" if minutes > 0
+    "#{hours_text}#{minutes_text}"
+  end
+
+  def process_notification(schedule:, text:)
     user = schedule.user
-    start_at = schedule.start_at.strftime('%H:%M')
-    text = "#{start_at}: #{schedule.summary} 🕊️"
 
     begin
       uid = user.profile_for(:line)&.uid
